76fb59f128af42a9de44d19283ef559a0aff32b2
Dhiva Ryan H <176643369+d-hardine@users.noreply.github.com>
2026-02-11 09:42:14 +0700

    private chat page and its chat aesthetics and functions pretty much done, need testing

--- src/pages/PrivateChat.jsx
+++ src/pages/PrivateChat.jsx
@@ -1,14 +1,16 @@
+import './PrivateChat.css'
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import Image from "react-bootstrap/Image"
 import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button"
+import Card from "react-bootstrap/Card"
 import Sidebar from "../components/Sidebar"
 import NavigationBar from "../components/NavigationBar"
 import BottomNavigationBar from "../components/BottomNavigationBar"
 import { useParams } from "react-router-dom"
-import { useState, useEffect, useContext } from "react"
+import { useState, useEffect, useContext, useRef } from "react"
 import UserContext from "../config/UserContext"
 import axiosInstance from "../config/axiosInstance"
 import socket from "../socket"
@@ -18,6 +20,8 @@ function PrivateChat() {
   const params = useParams()
 
   const { user } = useContext(UserContext)
+
+  const messagesEndRef = useRef(null)
 
   const [members, setMembers] = useState()
   const [messages, setMessages] = useState([])
@@ -48,6 +52,10 @@ function PrivateChat() {
     }
   }
 
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
+  }
+
   useEffect(() => {
     retrieveMessages()
     retrieveConversationMembers()
@@ -61,9 +69,12 @@ function PrivateChat() {
     })
   }, [socket])
 
+  useEffect(() => scrollToBottom, [messages])
+
   const handleNewMessage = (e) => {
     e.preventDefault()
-    console.log(newMessage)
+    setNewMessage("")
+    e.target.reset()
     socket.emit('send_message', {
       conversationId: params.conversationId,
       senderId: user.id,
@@ -82,25 +93,37 @@ function PrivateChat() {
             <Sidebar />
           </Col>
           <Col>
-            {messages.length > 0 ? (
-              <>
-                <h2 className="d-flex gap-2">
-                  <Image src={members[0].user.profilePic} className="object-fit-cover mt-1" width='35px' height='35px' roundedCircle />
-                  {members[0].user.name}
-                </h2>
-                {messages.map(message => (
-                  <div key={message.id}>{message.sender.name}: {message.content}</div>
-                ))}
-                <Form onSubmit={handleNewMessage}>
-                  <Form.Group className="mb-3" controlId="new-message-form">
-                    <Form.Control type="text" onChange={(e) => setNewMessage(e.target.value)} required />
-                  </Form.Group>
-                  <Button type="submit">Send</Button>
-                </Form>
-              </>
-            ) : (
-              <div>empty chat</div>
-            )}
+            <Row>
+            <h2 className="d-flex gap-2 mb-4">
+              <Image src={members[0].user.profilePic} className="object-fit-cover mt-1" width='35px' height='35px' roundedCircle />
+              {members[0].user.name}
+            </h2>
+            <div className="messages-container mb-3" style={{ maxHeight: '670px', overflowY: 'auto' }}>
+              {messages.map(message => (
+                <div key={message.id} className={`d-flex mb-2  ${message.senderId !== user.id ? 'justify-content-start' : 'justify-content-end'}`}>
+                  <Card className={`chat-bubble ${message.senderId !== user.id ? 'bg-light text-dark' : 'bg-success text-white'}`}>
+                    <Card.Body className="p-2">
+                      {message.content}
+                    </Card.Body>
+                  </Card>
+                </div>
+              ))}
+              {/* Invisible div to scroll to */}
+              <div ref={messagesEndRef} />
+            </div>
+            </Row>
+            <Form onSubmit={handleNewMessage}>
+              <Row>
+              <Col className='col-10 col-lg-11'>
+                <Form.Group className="mb-3" controlId="new-message-form">
+                  <Form.Control autoComplete='off' type="text" onChange={(e) => setNewMessage(e.target.value)} required />
+                </Form.Group>
+              </Col>
+              <Col className='col-1'>
+                <Button type="submit">Send</Button>
+              </Col>
+              </Row>
+            </Form>
           </Col>
         </Row>
       </Container>
