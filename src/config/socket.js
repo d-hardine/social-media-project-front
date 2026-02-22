9a0b52d8b7464680693105fd4dc24aee58d5e84e
Dhiva Ryan H <176643369+d-hardine@users.noreply.github.com>
2026-02-22 12:03:07 +0700

    modified the socket js because netlify _redirects only supports HTTP/HTTPS

--- src/config/socket.js
+++ src/config/socket.js
@@ -1,6 +1,10 @@
 import { io } from 'socket.io-client'
 
-const url = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000'
-const socket = io(url, {autoConnect: false})
+const url = !import.meta.env.VITE_API_BASE_URL ? 'http://localhost:3000' : 'https://chubby-poppy-hardine-d51d6d84.koyeb.app'
+const socket = io(url, {
+    autoConnect: false,
+    withCredentials: true,
+    transports: ["polling", "websocket"]
+})
 
 export default socket
